85eff482941ca8bc05cd83bdd000b6a46d7bc61b
emiliamassing <emiliamassings@gmail.com>
2023-05-29 15:47:13 +0200

    added player form

---
+++ src/components/AddPlayer.vue
@@ -0,0 +1,34 @@
+<script setup lang='ts'>
+    const addPlayerForm: string = 'addPlayerForm';
+    const addPlayerBtn: string = 'addPlayerBtn';
+</script>
+
+<template>
+    <form :class='addPlayerForm'>
+        <span>Player name: </span>
+        <label for="name">
+            <input type='text' name='player'>
+        </label>
+        <span>Play as: </span>
+        <label for='name'>
+            <input type='radio' name='shape' value='X'>
+            X
+        </label>
+        <label for='shape'>
+            <input type='radio' name='shape' value='O'>
+            O
+        </label>
+        <button :class="addPlayerBtn">Add Player</button>
+    </form>
+</template>
+
+<style lang='scss'>
+    .addPlayerForm {
+        display: flex;
+        flex-direction: column;
+
+        label {
+            margin-bottom: 1em;
+        }
+    }
+</style>
