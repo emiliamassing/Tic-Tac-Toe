8f80229a2e4fcb17c8846e1a72115637e5bc9fbf
emiliamassing <emiliamassings@gmail.com>
2023-06-01 13:22:26 +0200

    🐛 🎨 Better Structure for adding players, localstorage problem solved

--- src/components/AddPlayer.vue
+++ src/components/AddPlayer.vue
@@ -7,7 +7,7 @@
     const addPlayerBtn: string = 'addPlayerBtn';
 
     let startGame = ref(false);
-    let players = ref<Player[]>([]);
+    const players = ref<Player[]>([]);
 
     let playerNameX = ref('');
     let playerNameO = ref('');
@@ -18,33 +18,9 @@
         const parse = JSON.parse(localPlayerList);
     };
 
-    /*function addToLocalStorage(playerObject: Player) {
-        let localPlayerList = localStorage.getItem('playerList') || [];
-
-        if(typeof localPlayerList === 'string') {
-            const parse = JSON.parse(localPlayerList);
-        };
-
-        [localPlayerList].push(JSON.stringify(playerObject));
-
-        localStorage.setItem('playerList', JSON.stringify(playerObject));
-    };*/
-
-    function addPlayer() {
-
-        if(players.value.length === 0) {
-            const newPlayerX = new Player(playerNameX.value, 'X', true);
-            players.value.push(newPlayerX);
-            
-            [localPlayerList].push(JSON.stringify(newPlayerX));
-            localStorage.setItem('playerList', JSON.stringify(newPlayerX));
-        } else {
-            const newPlayerO = new Player(playerNameO.value, 'O', false);
-            players.value.push(newPlayerO);
-
-            [localPlayerList].push(JSON.stringify(newPlayerO));
-            localStorage.setItem('playerList', JSON.stringify(newPlayerO));
-        };
+    function addPlayers() {
+        players.value.push(new Player(playerNameX.value, 'X'), new Player(playerNameO.value, 'O'));
+        localStorage.setItem('playerList', JSON.stringify(players.value));
 
         checkplayersLength();
     };
@@ -58,19 +34,19 @@
 
 <template>
     <div :class="addPlayerContainer" v-if="!startGame">
-        <div class="playerX" v-if="players.length === 0">
+        <div class="playerX">
             <span>Player name X: </span>
             <label for="name">
                 <input type='text' name='player' v-model="playerNameX">
             </label>
         </div>
-        <div class="playerO" v-else-if="players.length === 1">
+        <div class="playerO">
             <span>Player name O: </span>
             <label for="name">
             <input type='text' name='player' v-model="playerNameO">
         </label>
         </div>
-        <button :class="addPlayerBtn" @click="addPlayer">Add Player</button>
+        <button :class="addPlayerBtn" @click="addPlayers">Add Player</button>
     </div>
     <template v-else>
         <GameplayView :player="players"></GameplayView>
--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,7 +1,6 @@
 export default class Player {
-    constructor(public name: string, public shape: string, public isPlaying: boolean){
+    constructor(public name: string, public shape: string){
         this.name = name;
         this.shape = shape;
-        this.isPlaying = isPlaying;
     };
 };
